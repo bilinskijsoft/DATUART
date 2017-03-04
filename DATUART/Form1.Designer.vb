11c663edd79d84bcde1519bf3d8bb01cba60ab73
bilinskijsoft <bilinskij.yaroslav@gmail.com>
2017-03-05 00:59:26 +0200

    Добавьте файлы проекта.

---
+++ DATUART/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.list = New System.Windows.Forms.ListView()
+        Me.chName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chValue = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chPercent = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(117, 366)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(40, 20)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("", System.UriKind.Relative)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(14, 366)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Запуск"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(213, 366)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Стоп"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'list
+        '
+        Me.list.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chName, Me.chValue, Me.chPercent})
+        Me.list.Location = New System.Drawing.Point(13, 115)
+        Me.list.Name = "list"
+        Me.list.Size = New System.Drawing.Size(275, 245)
+        Me.list.TabIndex = 3
+        Me.list.UseCompatibleStateImageBehavior = False
+        Me.list.View = System.Windows.Forms.View.Details
+        '
+        'chName
+        '
+        Me.chName.Text = "Name"
+        '
+        'chValue
+        '
+        Me.chValue.Text = "Value"
+        '
+        'chPercent
+        '
+        Me.chPercent.Text = "Percent"
+        '
+        'Timer1
+        '
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200"})
+        Me.ComboBox1.Location = New System.Drawing.Point(95, 34)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(193, 21)
+        Me.ComboBox1.TabIndex = 4
+        Me.ComboBox1.Text = "9600"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(95, 6)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(193, 21)
+        Me.ComboBox2.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Порт"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Скорость"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(147, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(141, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "5"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Интервал обновления, с"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "URL"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(45, 89)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(242, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(300, 397)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.list)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "DATUART"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents list As ListView
+    Friend WithEvents chName As ColumnHeader
+    Friend WithEvents chValue As ColumnHeader
+    Friend WithEvents chPercent As ColumnHeader
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ DATUART/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    ''' <summary>
+    ''' Структура пункта гоосования
+    ''' </summary>
+    Public Structure PoolVariantData
+        Dim Name As String
+        Dim Value As Integer
+        Dim Percent As Integer
+    End Structure
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.Open()
+
+        If TextBox2.Text = "" Then
+            MsgBox("Введите ссылку!")
+            Exit Sub
+        End If
+        Me.Text = "DATUART - Working..."
+        Timer1.Enabled = True
+
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        ComboBox1.Enabled = False
+        ComboBox2.Enabled = False
+
+    End Sub
+
+    ''' <summary>
+    ''' Поиск вариантов голосования 
+    ''' </summary>
+    ''' <param name="HTMl">HTML код</param>
+    ''' <returns>Возвращает колекцию совпадений поиска вариантов голосования</returns>
+    Function getPoolVariants(HTMl As String) As MatchCollection
+        Return Regex.Matches(HTMl, "<div class=""poll-variant-container"" data-optionid="".*"">\n.*")
+    End Function
+
+    ''' <summary>
+    ''' Поучаем данные с варианта голосования
+    ''' </summary>
+    ''' <param name="HTML">HTML код</param>
+    ''' <returns>Возвращает структуру данных</returns>
+    Function getDataFromVariant(HTML As String) As PoolVariantData
+        Dim result As PoolVariantData = New PoolVariantData
+        Dim m1 As MatchCollection = Regex.Matches(HTML, "(?!>)([а-яА-ЯёЁa-zA-Z0-9])+(?=<)")
+
+        result.Name = m1.Item(0).ToString
+        result.Value = Int(m1.Item(1).ToString)
+        result.Percent = Int(m1.Item(2).ToString)
+
+        Return result
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Close()
+        Me.Text = "DATUART"
+        Timer1.Enabled = False
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        ComboBox1.Enabled = True
+        ComboBox2.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Ports = IO.Ports.SerialPort.GetPortNames()
+        SerialPort1.Encoding = System.Text.Encoding.GetEncoding(1251)
+        For Each port In Ports
+            ComboBox2.Items.Add(port)
+        Next
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Try
+            SerialPort1.BaudRate = Int(ComboBox1.Text)
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString(), MsgBoxStyle.Critical, "ERROR")
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        Try
+            SerialPort1.PortName = ComboBox2.Text
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString(), MsgBoxStyle.Critical, "ERROR")
+        End Try
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text <> "" Then
+            Timer1.Interval = TextBox1.Text * 1000
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text <> "" Then
+            Try
+                WebBrowser1.Url = New Uri(TextBox2.Text)
+            Catch ex As Exception
+
+            End Try
+
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim elements As HtmlElementCollection
+        elements = WebBrowser1.Document.GetElementsByTagName("HTML")
+        Dim htmlElement = elements(0)
+        Dim pageSource = htmlElement.OuterHtml
+
+        Dim Pool As MatchCollection
+        Pool = getPoolVariants(pageSource)
+
+        Dim poolVariantData As PoolVariantData
+
+        list.Items.Clear()
+
+        For Each poolVariant In Pool
+            poolVariantData = getDataFromVariant(poolVariant.ToString)
+
+            Try
+                SerialPort1.Write(poolVariantData.Name & "-" & poolVariantData.Value & ";" + vbCrLf)
+            Catch ex As Exception
+                MsgBox(ex.Message.ToString, MsgBoxStyle.Critical, "ERROR")
+            End Try
+
+
+            list.Items.Add(poolVariantData.Name)
+            list.Items.Item(list.Items.Count - 1).SubItems.Add(poolVariantData.Value)
+            list.Items.Item(list.Items.Count - 1).SubItems.Add(poolVariantData.Percent)
+        Next
+        WebBrowser1.Refresh()
+    End Sub
+End Class
